e7d4f42144fa66262104680edfa1ea8ad5582aee
epsilone <ben_epsilone@hotmail.com>
2019-10-14 02:23:37 +0000

    random choice sounds

--- simple.py
+++ simple.py
@@ -1,27 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 import pygame
+from random import choice
+
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 
-sound_template = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine%s-loud.wav"
-sounds = [sound_template % i for i in ["1", "2"]]
-sound = sounds[0]
+filesound1 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine1-loud.wav"
+filesound2 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine2-loud.wav"
 
 RELAIS_SPRAY = 17
 MAT_INPUT = 4
 try:
     GPIO.setup(RELAIS_SPRAY, GPIO.OUT)
     GPIO.setup(MAT_INPUT, GPIO.IN)
-    pygame.mixer.init()
-    pygame.mixer.music.load(sound)
-    GPIO.output(RELAIS_SPRAY, GPIO.LOW) # on
+    GPIO.output(RELAIS_SPRAY, GPIO.LOW) # off
+    pygame.mixer.init()    
+    sound1 = pygame.mixer.Sound(file=filesound1)
+    sound2 = pygame.mixer.Sound(file=filesound2)
+    sounds = [sound1, sound2]
+    for s in sounds:
+        s.set_volume(1)
     while True:
+        print("waiting")
         while not GPIO.input(MAT_INPUT):
             time.sleep(0.2)
-        pygame.mixer.music.play()
-        time.sleep(4.5)
+        print("a client")
+        time.sleep(10)
+        # if the trigger left after 10s
+        if not GPIO.input(MAT_INPUT):
+            # Back to waiting
+            print("client left")
+            continue
+        # The subject must be in place by now.
+        sound = choice(sounds)
+        sound_length = sound.get_length()
+        sound.play()
+        time.sleep(sound_length - 0.5)  # delay of the sound
+        # .5s of full blast of air in the nect
+        print("bam")
         GPIO.output(RELAIS_SPRAY, GPIO.HIGH) # on
         time.sleep(0.5)
-        GPIO.output(RELAIS_SPRAY, GPIO.LOW) # on
+        GPIO.output(RELAIS_SPRAY, GPIO.LOW) # off
 except:  
     GPIO.cleanup()  
