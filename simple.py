31a550df5209c03ab075466b3e80a65d8fe14fe3
Benjamin Bounous <ben_epsilone@hotmail.com>
2019-10-14 01:37:40 +0000

    loud sound new logic
    Now the client has to stay on the mat for 10s to trigger the sound and blast.

--- simple.py
+++ simple.py
@@ -3,9 +3,7 @@ import time
 import pygame
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 
-sound_template = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine%s-loud.wav"
-sounds = [sound_template % i for i in ["1", "2"]]
-sound = sounds[0]
+sound = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine2-loud.wav"
 
 RELAIS_SPRAY = 17
 MAT_INPUT = 4
@@ -16,12 +14,23 @@ try:
     pygame.mixer.music.load(sound)
     GPIO.output(RELAIS_SPRAY, GPIO.LOW) # on
     while True:
+        print("waiting")
         while not GPIO.input(MAT_INPUT):
             time.sleep(0.2)
+        print("a client")
+        time.sleep(10)
+        # if the trigger left after 10s
+        if not GPIO.input(MAT_INPUT):
+            # Back to waiting
+            print("client left")
+            continue
+        # The subject must be in place by now.
         pygame.mixer.music.play()
-        time.sleep(4.5)
+        time.sleep(2.1)  # delay of the sound
+        # .5s of full blast of air in the nect
+        print("bam")
         GPIO.output(RELAIS_SPRAY, GPIO.HIGH) # on
         time.sleep(0.5)
-        GPIO.output(RELAIS_SPRAY, GPIO.LOW) # on
+        GPIO.output(RELAIS_SPRAY, GPIO.LOW) # off
 except:  
     GPIO.cleanup()  
