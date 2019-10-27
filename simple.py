eb1a18ec60e0420d0d022537995bd6ba8e264eb5
epsilone <ben_epsilone@hotmail.com>
2019-10-27 14:44:46 +0000

    adding better timer

--- simple.py
+++ simple.py
@@ -5,8 +5,10 @@ from random import choice
 
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 
-filesound1 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine1-loud.wav"
-filesound2 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine2-loud.wav"
+filesound1 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine1-normalized.wav"
+filesound2 = "/home/pi/Desktop/ScaryDahlia/MassicotStreamer/guillotine2-normalized.wav"
+
+TIME_BEFORE_SOUND = 7
 
 RELAIS_SPRAY = 17
 MAT_INPUT = 4
@@ -25,17 +27,22 @@ try:
         while not GPIO.input(MAT_INPUT):
             time.sleep(0.2)
         print("a client")
-        time.sleep(10)
-        # if the trigger left after 10s
-        if not GPIO.input(MAT_INPUT):
-            # Back to waiting
-            print("client left")
+        client = True
+        for i in range(0, TIME_BEFORE_SOUND):
+            time.sleep(1)
+            # if the trigger left after 10s
+            if not GPIO.input(MAT_INPUT):
+                # Back to waiting
+                print("client left")
+                client = False
+                break
+        if not client:
             continue
         # The subject must be in place by now.
         sound = choice(sounds)
         sound_length = sound.get_length()
         sound.play()
-        time.sleep(sound_length - 0.5)  # delay of the sound
+        time.sleep(sound_length - 1)  # delay of the sound -1s padding
         # .5s of full blast of air in the nect
         print("bam")
         GPIO.output(RELAIS_SPRAY, GPIO.HIGH) # on
